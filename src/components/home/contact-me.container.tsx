46122b89e224e254e88137902758b5f070446b1e
m.pakravan <m.pakravan@nizek.com>
2025-01-07 16:49:13 +0330

    feat / handle contact me

--- src/components/home/contact-me.container.tsx
+++ src/components/home/contact-me.container.tsx
@@ -1,27 +1,7 @@
-import { useTranslations } from 'next-intl'
 import ContactMeSection from './contact-me'
 
 function ContactMeContainer() {
-  const t = useTranslations('ContactMeSection')
-  const translations = {
-    contactMeTitleWhite: t('contactMeTitleWhite'),
-    contactMeTitleGreen: t('contactMeTitleGreen'),
-    contactUs: t('contactUs'),
-    developDream: t('developDream'),
-    name: t('name'),
-    phoneNumber: t('phoneNumber'),
-    message: t('message'),
-    placeholderName: t('placeholderName'),
-    placeholderPhone: t('placeholderPhone'),
-    placeholderMessage: t('placeholderMessage'),
-    button: t('button'),
-    loremText: t('loremText'),
-    phone: t('phone'),
-    instagram: t('instagram'),
-    github: t('github'),
-  }
-
-  return <ContactMeSection t={translations} />
+  return <ContactMeSection/>
 }
 
 export default ContactMeContainer
--- src/components/home/contact-me.tsx
+++ src/components/home/contact-me.tsx
@@ -3,9 +3,10 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { GithubIcon, InstagramIcon, SmartphoneIcon } from 'lucide-react'
 import Image from 'next/image'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
+import { useTranslations } from 'next-intl'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -46,8 +47,11 @@ const CustomInput = ({
   </div>
 )
 
-const ContactMeSection = ({ t }: { t: Record<string, any> }) => {
+const ContactMeSection = () => {
   const containerRef = useRef<HTMLDivElement>(null)
+  const t = useTranslations('ContactMeSection')
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSuccess, setIsSuccess] = useState(false)
 
   useEffect(() => {
     const context = gsap.context(() => {
@@ -75,35 +79,81 @@ const ContactMeSection = ({ t }: { t: Record<string, any> }) => {
     }
   }, [])
 
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault()
+      setIsLoading(true)
+
+      const inputs = {
+        name: (e.currentTarget.name as any)?.value,
+        email: (e.currentTarget.phone as any)?.value,
+        message: (e.currentTarget.message as any)?.value,
+      }
+
+      if (!inputs.name || !inputs.email || !inputs.message) {
+        setIsLoading(false)
+        return
+      }
+
+      const res = await fetch('https://api.mpakravan.com/api/collections/contact/records', {
+        method: 'POST',
+        body: JSON.stringify(inputs),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const resp = await res.json()
+      console.log(resp)
+      setIsLoading(false)
+      setIsSuccess(true)
+    } catch (error) {
+      console.error(error)
+      setIsLoading(false)
+      setIsSuccess(false)
+    }
+  }
+
   return (
     <section className="container mt-24 mb-12" id="contact">
       <h2 className="text-left text-[36px] font-bold">
-        <span className="text-primary">{t.contactMeTitleWhite} </span>
-        {t.contactMeTitleGreen}
+        <span className="text-primary">{t('contactMeTitleWhite')} </span>
+        {t('contactMeTitleGreen')}
       </h2>
 
-      <div className="flex flex-col lg:flex-row h-auto lg:h-[582px] mt-4">
+      <div className="flex flex-col lg:flex-row h-auto  mt-4">
         {/* Contact Form Section */}
         <div className="w-full lg:w-2/5 bg-[rgba(110,125,135,.1)] rounded-tl-md rounded-bl-md p-6">
-          <span className="text-md block opacity-80">{t.contactUs}</span>
-          <span className="text-2xl font-bold block mt-3">{t.developDream}</span>
+          <span className="text-md block opacity-80">{t('contactUs')}</span>
+          <span className="text-2xl font-bold block mt-3">{t('developDream')}</span>
 
-          <form className="mt-6 flex flex-col gap-6">
-            <CustomInput label={t.name} placeholder={t.placeholderName} type="text" name="name" />
+          <form className="mt-6 flex flex-col gap-6" onSubmit={onSubmit}>
             <CustomInput
-              label={t.phoneNumber}
-              placeholder={t.placeholderPhone}
+              label={t('name')}
+              placeholder={t('placeholderName')}
+              type="text"
+              name="name"
+            />
+            <CustomInput
+              label={t('phoneNumber')}
+              placeholder={t('placeholderPhone')}
               type="text"
               name="phone"
             />
             <CustomInput
-              label={t.message}
-              placeholder={t.placeholderMessage}
+              label={t('message')}
+              placeholder={t('placeholderMessage')}
               type="text"
               name="message"
               textArea
             />
-            <Button variant="outlinePrimary">{t.button}</Button>
+            {isSuccess && (
+              <span className="text-primary font-semibold fade-in-10">
+                Your Message has Successfully sent to me !
+              </span>
+            )}
+            <Button variant="outlinePrimary" disabled={isLoading || isSuccess}>
+              {isLoading ? t('loading') : t('button')}
+            </Button>
           </form>
         </div>
 
@@ -112,14 +162,14 @@ const ContactMeSection = ({ t }: { t: Record<string, any> }) => {
           <Image src="/images/contact-bg.png" fill alt={'ALT'} className="block" />
 
           <p className="text-md rtl:text-lg rtl:leading-10 opacity-80 w-[86%] leading-8 mt-6">
-            {t.loremText}
+            {t('loremText')}
           </p>
 
           <div className="w-[86%] flex justify-between mt-12">
             {[
-              { icon: SmartphoneIcon, label: t.phone, value: '+98 9369514975' },
-              { icon: InstagramIcon, label: t.instagram, value: 'mpakravan.ir' },
-              { icon: GithubIcon, label: t.github, value: 'mahdipakravan-dev' },
+              { icon: SmartphoneIcon, label: t('phone'), value: '+98 9369514975' },
+              { icon: InstagramIcon, label: t('instagram'), value: 'mpakravan.ir' },
+              { icon: GithubIcon, label: t('github'), value: 'mahdipakravan-dev' },
             ].map(({ icon: Icon, label, value }, index) => (
               <div key={index} className="flex">
                 <Icon width={40} height={40} className="mr-1 pt-2 text-white" />
